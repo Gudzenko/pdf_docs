7eb1915956c7de55b170cac64eda87e9ffb32ce3
Gudzenko <o.gudzenko@weegree.com>
2021-07-05 11:50:19 +0200

    add main and style

---
+++ main.py
@@ -0,0 +1,60 @@
+import pdfkit
+
+# for windows install package from https://wkhtmltopdf.org/downloads.html
+
+config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
+body = """
+    <html>
+      <body>
+        <div id="header"></div>
+        <div class="left"></div>
+        <div class="stuff">
+          <br><br>
+          <h1>Resume</h1>
+          <h2>{name}</h2>
+          <hr />
+          <br>
+          <p class="head">Interests</p>
+          <ul>
+            <li>Drawing</li>
+            <li>Photography</li>
+            <li>Design</li>
+            <li>Programming</li>
+            <li>Computer Science</li>
+          </ul>
+          <p class="head">Skills</p>
+          <ul>
+            <li>Web Design with HTML & CSS</li>
+          </ul>
+          <p class="head">Education</p>
+          <ul>
+            <a href="http://www.wiltonhighschool.org/pages/Wilton_High_School">
+              <li>Wilton High School</li>
+            </a>
+            <!--Link-->
+            <a href="https://www.silvermineart.org/">
+              <li>Silvermine School of Arts</li>
+            </a>
+            <li>Codeacademy</li>
+          </ul>
+          <p class="head">Experience</p>
+          <ul>
+            <li>Student Technology Intern for Wilton School District</li>
+            <li>Babysitter</li>
+          </ul>
+          <p class="head">Extracurriculars</p>
+          <ul>
+            <li>Recycling Club</li>
+            <li>Gardening Club</li>
+            <li>Book Club</li>
+          </ul>
+        </div>
+        <div class="right"></div>
+        <div id="footer">
+          <h2 id="name">{name}</h2>
+        </div>
+    </body>
+    </html>
+    """.format(name="John Smith")
+css = 'style.css'
+pdfkit.from_string(body, 'cv.pdf', configuration=config, css=css)
